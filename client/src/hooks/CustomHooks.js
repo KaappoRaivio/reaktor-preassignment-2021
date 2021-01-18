82392f51764bc7a74fdda9fd5870c39daa7651a5
Kaappo Raivio <kaappo.raivio@gmail.com>
2021-01-18 19:41:26 +0200

    chore: move custom hooks to a dedicated file from Main.jsx
    Took 4 minutes

---
+++ client/src/hooks/CustomHooks.js
@@ -0,0 +1,92 @@
+import { useEffect, useRef, useState } from "react";
+
+const useRequest = (url, { retries = 3 } = {}) => {
+	const [retriesLeft, setRetriesLeft] = useState(retries);
+	const [waiting, setWaiting] = useState(true);
+	const [JSON, setJSON] = useState(null);
+	const [error, setError] = useState(null);
+
+	const [didCancel, setDidCancel] = useState(false);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const response = await fetch(url);
+			if (!didCancel) {
+				if (response.status < 200 || response.status >= 300) {
+					if (retriesLeft > 0) {
+						setRetriesLeft(retriesLeft => retriesLeft - 1);
+						return fetchData();
+					} else {
+						setError({ status: response.status, error: response.statusText });
+						setWaiting(false);
+						return;
+					}
+				}
+
+				const JSON = await response.json();
+				setJSON(JSON);
+				setWaiting(false);
+			}
+		};
+		fetchData();
+
+		return () => {
+			setDidCancel(true);
+		};
+	}, [url, didCancel, retriesLeft]);
+
+	return { waiting, JSON, error };
+};
+
+const useInterval = (callback, delay) => {
+	const savedCallback = useRef();
+	useEffect(() => {
+		savedCallback.current = callback;
+	}, [callback]);
+
+	useEffect(() => {
+		const tick = () => savedCallback.current();
+
+		if (delay != null) {
+			const id = setInterval(tick, delay);
+			return () => clearInterval(id);
+		}
+	}, [callback, delay]);
+};
+
+const usePollingRequest = pollingInitialisationURL => {
+	const { waiting: pollingInitialisationWaiting, JSON, error: pollingInitializationError } = useRequest(
+		pollingInitialisationURL
+	);
+	const [UUID, setUUID] = useState(null);
+	const [finished, setFinished] = useState(false);
+	const [data, setData] = useState(null);
+	const [error, setError] = useState(null);
+
+	if (!pollingInitialisationWaiting && UUID == null) {
+		setUUID(JSON.uuid);
+	}
+
+	useInterval(
+		async () => {
+			if (UUID) {
+				const response = await fetch(`${API_ENDPOINT}/api/jobs/${UUID}`);
+				if (response.status < 200 || response.status >= 300) {
+					return;
+				}
+				const { finished, hasNewData, data } = await response.json();
+				// console.log(finished, hasNewData, data);
+				setFinished(finished);
+
+				if (hasNewData) {
+					setData(data);
+				}
+			}
+		},
+		finished ? null : 1000
+	);
+
+	return { waiting: data == null, JSON: data, error: error || pollingInitializationError };
+};
+
+export { useRequest, usePollingRequest };
